be3152839f6777b11cd8966580ef55ea6acb1083
russelibale808 <russelibale808@gmail.com>
2022-11-07 10:50:57 -1000

    LINQ-Exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LINQ
+﻿using System.Transactions;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGameList = new List<string>
+            { "StarWars", "Paper Boy", "Battlefield Four", "Super Mario Three", "Dig Dug"};
+
+            IEnumerable<string> length = videoGameList.OrderBy(x => x.Length).ToList();
+
+            foreach (string videoGame in length)
+            {
+                Console.WriteLine(videoGame);
+            }
         }
     }
 }
